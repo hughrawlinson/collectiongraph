152dfcf50156f88db665c8647208d595b851979a
Hugh Rawlinson <hughrawlinson@users.noreply.github.com>
2019-04-17 16:48:09 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   clientId: "d048d884a131460d851c9d609e2e1b53",
-  clientSecret: "5d17eb21c1f74c308f8acfb828b20529",
-  accessToken: "BQAoPBluScL7hGMAu-d_dh3zW4WPV0C0njPwztgcxm3Yr9iF2a7pPxgztcoY8pBj3pvckG7lCZOAirMG3LX4lNIMTQMkD542WQYGNrC5GaxgJm2q1hLFDAYMWtdstdfjVftLHqEjEmuqOCQhbGY5TIEI80Juwf3cS9Xmx3v3zX96cWe6uYg",
+  clientSecret: "I cycled this secret, don't bother searching Git History",
+  accessToken: "The token expired LONG ago too :)",
   redirectUri: null,
 }
